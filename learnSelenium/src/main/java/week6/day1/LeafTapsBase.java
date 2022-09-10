34af47beddd357fff9acbed5cc72a902df40d217
Uma Shankar Nallaiah <nallaiahu@cardiff.ac.uk>
2022-09-10 18:12:40 +0100

    week6day1assignment1

---
+++ learnSelenium/src/main/java/week6/day1/LeafTapsBase.java
@@ -0,0 +1,41 @@
+package week6.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LeafTapsBase {
+
+	public WebDriver driver;
+
+//	public LeafTapsBase(WebDriver driver) {
+//		// TODO Auto-generated constructor stub
+//		this.driver = new ChromeDriver();
+//	}
+
+	@BeforeMethod
+	public void preCondition() {
+		WebDriverManager.chromedriver().setup();
+		//ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+	}
+	
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+	}
+}
